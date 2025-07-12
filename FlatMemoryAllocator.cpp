ac0ebc3a1e4608e3208c66f2d4637acc9283bfe1
lelepods <leighalbo23@gmail.com>
2025-07-12 14:28:07 +0800

    minor changes

--- FlatMemoryAllocator.cpp
+++ FlatMemoryAllocator.cpp
@@ -9,11 +9,6 @@
 #include <iomanip>
 #include <memory>
 
-/**
- * @brief Constructor for FlatMemoryAllocator.
- * @param maximum_size The total size of the memory pool.
- * @param mem_per_frame The size of each memory frame.
- */
 FlatMemoryAllocator::FlatMemoryAllocator(size_t maximum_size, size_t mem_per_frame)
     : maximum_size(maximum_size), allocated_size(0), memory(maximum_size, '.'),
     allocation_map(maximum_size, false), mem_per_frame(mem_per_frame), n_process(0)
@@ -21,9 +16,6 @@ FlatMemoryAllocator::FlatMemoryAllocator(size_t maximum_size, size_t mem_per_fra
     initializeMemory();
 }
 
-/**
- * @brief Destructor for FlatMemoryAllocator.
- */
 FlatMemoryAllocator::~FlatMemoryAllocator()
 {
     std::lock_guard<std::mutex> lock(memory_mutex);
@@ -31,11 +23,6 @@ FlatMemoryAllocator::~FlatMemoryAllocator()
     allocation_map.clear();
 }
 
-/**
- * @brief Allocates memory for a process.
- * @param process Shared pointer to the process requesting memory.
- * @return Pointer to the allocated memory block.
- */
 void* FlatMemoryAllocator::allocate(std::shared_ptr<Process> process)
 {
     size_t size = process->getMemoryRequired();
@@ -55,13 +42,9 @@ void* FlatMemoryAllocator::allocate(std::shared_ptr<Process> process)
             return reinterpret_cast<void*>(&memory[block_start]);
         }
     }
-    return nullptr;  // No sufficient contiguous block found
+    return nullptr;
 }
 
-/**
- * @brief Deallocates memory for a process.
- * @param process Shared pointer to the process whose memory is to be deallocated.
- */
 void FlatMemoryAllocator::deallocate(std::shared_ptr<Process> process)
 {
     std::lock_guard<std::mutex> lock(memory_mutex);
@@ -76,9 +59,6 @@ void FlatMemoryAllocator::deallocate(std::shared_ptr<Process> process)
     }
 }
 
-/**
- * @brief Visualizes the current memory allocation state.
- */
 void FlatMemoryAllocator::visualizeMemory()
 {
     std::lock_guard<std::mutex> lock(memory_mutex);
@@ -90,9 +70,6 @@ void FlatMemoryAllocator::visualizeMemory()
     std::cout << std::endl;
 }
 
-/**
- * @brief Initializes memory and allocation map.
- */
 void FlatMemoryAllocator::initializeMemory()
 {
     std::lock_guard<std::mutex> lock(memory_mutex);
@@ -102,12 +79,6 @@ void FlatMemoryAllocator::initializeMemory()
     free_blocks[0] = maximum_size;
 }
 
-/**
- * @brief Checks if memory can be allocated at a specific index.
- * @param index The index to check.
- * @param size The size of memory to allocate.
- * @return True if memory can be allocated at the index, false otherwise.
- */
 bool FlatMemoryAllocator::canAllocateAt(size_t index, size_t size) const
 {
     auto it = free_blocks.lower_bound(index);
@@ -118,11 +89,6 @@ bool FlatMemoryAllocator::canAllocateAt(size_t index, size_t size) const
     return true;
 }
 
-/**
- * @brief Allocates memory starting at a given index.
- * @param index The starting index of the block to allocate.
- * @param size The size of the block to allocate.
- */
 void FlatMemoryAllocator::allocateAt(size_t index, size_t size)
 {
     auto it = free_blocks.lower_bound(index);
@@ -145,11 +111,6 @@ void FlatMemoryAllocator::allocateAt(size_t index, size_t size)
     }
 }
 
-/**
- * @brief Frees an allocated block of memory starting at index.
- * @param index The starting index of the block to deallocate.
- * @param size The size of the block to deallocate.
- */
 void FlatMemoryAllocator::deallocateAt(size_t index, size_t size)
 {
     auto next = free_blocks.lower_bound(index);
@@ -175,40 +136,24 @@ void FlatMemoryAllocator::deallocateAt(size_t index, size_t size)
     allocated_size -= size;
 }
 
-/**
- * @brief Gets the number of processes in memory.
- * @return The number of processes in memory.
- */
 int FlatMemoryAllocator::getNProcess()
 {
     std::lock_guard<std::mutex> lock(memory_mutex);
     return n_process;
 }
 
-/**
- * @brief Gets a list of all processes in memory.
- * @return A map of starting memory indices to process pointers.
- */
 std::map<size_t, std::shared_ptr<Process>> FlatMemoryAllocator::getProcessList()
 {
     std::lock_guard<std::mutex> lock(memory_mutex);
     return process_list;
 }
 
-/**
- * @brief Gets the maximum memory size of the allocator.
- * @return The maximum memory size.
- */
 size_t FlatMemoryAllocator::getMaxMemory()
 {
     std::lock_guard<std::mutex> lock(memory_mutex);
     return maximum_size;
 }
 
-/**
- * @brief Gets the current amount of external fragmentation.
- * @return The size of external fragmentation.
- */
 size_t FlatMemoryAllocator::getExternalFragmentation()
 {
     std::lock_guard<std::mutex> lock(memory_mutex);
@@ -239,10 +184,6 @@ size_t FlatMemoryAllocator::getExternalFragmentation()
     return total_free_space;
 }
 
-/**
- * @brief Deallocates the oldest process in memory to free up space.
- * @param mem_size The size of memory to free.
- */
 void FlatMemoryAllocator::deallocateOldest(size_t mem_size)
 {
     std::chrono::time_point<std::chrono::system_clock> oldest_time = std::chrono::time_point<std::chrono::system_clock>::max();
@@ -268,7 +209,7 @@ void FlatMemoryAllocator::deallocateOldest(size_t mem_size)
     {
         while (oldest_process->getState() == Process::ProcessState::RUNNING)
         {
-            // Wait until the process is not running
+            
         }
 
         std::ofstream backing_store("backingstore.txt", std::ios::app);
@@ -308,19 +249,11 @@ void FlatMemoryAllocator::deallocateOldest(size_t mem_size)
     }
 }
 
-/**
- * @brief Gets the number of page-ins that have occurred.
- * @return The number of page-ins.
- */
 size_t FlatMemoryAllocator::getPageIn()
 {
     return 0;
 }
 
-/**
- * @brief Gets the number of page-outs that have occurred.
- * @return The number of page-outs.
- */
 size_t FlatMemoryAllocator::getPageOut()
 {
     return 0;
